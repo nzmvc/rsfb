d19754bd3a35e1c44af28a4d50706a8c98410519
MacBook <macbook@macbooks-macbook-pro.local>
2016-03-01 12:34:48 +0200

    günün sorusu ve dunun sorusu gösterilebiliyor. harf sayısı gösterme çalışıyor plan ekleme cıkarma ok
    uyelik NOK

--- resfebe/ViewController.swift
+++ resfebe/ViewController.swift
@@ -10,14 +10,169 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let img_path_prefix = "http://resfebe.nazimavci.com/pictures/"
-    var img_file = ""
-    var degerArray = [[String]]()
+    let img_path_prefix     = "http://resfebe.nazimavci.com/pictures/"
+    var img_file            = ""
+    var degerArray          = [[String]]()
+    var cevap:String        = ""
+    var kullaniciCevap:String = ""
+    var harfler:String      = ""
+    var ilkHarf:Bool        = true
+    var harfSayisiGoster:Bool = false
+    var harfSayisi:Int      = 0
+    var girilenHarf:Int     = 0
+    var puan:Int            = 50
     
     @IBOutlet weak var image: UIImageView!
-    @IBAction func btnDununSorusu(sender: AnyObject) {
+    @IBOutlet weak var outPuan: UILabel!
+    
+    func uyariMesaji(title:String, str : String){
+        
+        let alert = UIAlertController(title: title, message: str, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        // show the alert
+        self.presentViewController(alert, animated: true, completion: nil)
         
     }
+    
+    
+    func cevapReset (){
+        
+        // yeniden deneme yapılacağı için sıfırlanır---------------------
+        ilkHarf = true
+        girilenHarf = 0
+        kullaniciCevap = ""
+        outYanit.text = ""
+        
+        if harfSayisiGoster {   // bu daha önce secilmişse resetlerken aynı secim devam eder
+            outYanit.text = "-"
+            for var i = 1 ; i < cevap.characters.count ; ++i {
+                outYanit.text = outYanit.text! + "  -"
+            }
+        }
+        
+   
+    }
+    func getImage(iPrefix:String, fileName: String) {
+        
+        let img_url = NSURL(string: iPrefix + fileName )
+        print(" aranan \(iPrefix) \(fileName) ")
+        
+        let task2 = NSURLSession.sharedSession().dataTaskWithURL(img_url!) { (data, response, error) -> Void in
+            
+            if error != nil {
+                print(error)
+            } else {
+                var documentsDirectory:String?
+                var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                
+                if paths.count > 0 {
+                    documentsDirectory = paths[0] as? String
+                    let savePath = documentsDirectory! + fileName
+                    NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.image.image = UIImage(named: savePath)
+                    })
+                }
+                
+                print("cevap: \(self.cevap) ")
+            }
+        }
+        
+        task2.resume()
+        
+    }
+    @IBAction func btnDununSorusu(sender: AnyObject) {
+        
+        cevapReset()
+    
+        self.img_file = self.degerArray[1][0]
+        self.cevap = self.degerArray[1][1]
+        self.harfSayisi = self.cevap.characters.count
+        
+        if self.img_file != "" {
+            
+            self.getImage(self.img_path_prefix, fileName: self.img_file)
+        
+        }
+        
+    }
+    
+    @IBAction func btnHarf(sender: AnyObject) {
+        
+        if ilkHarf  {
+            
+            ilkHarf = false                                 //ilk harf ise ekleme yapmıyorum
+            kullaniciCevap = (sender.titleLabel?!.text)!        //basılan butonun üzerindeki harfi alıyorum
+            harfSayisi = harfSayisi - 1
+            
+        } else {
+            
+            kullaniciCevap = kullaniciCevap + (sender.titleLabel?!.text)!
+        
+        }
+        
+        girilenHarf += 1
+        outYanit.text = kullaniciCevap
+        
+        
+        if harfSayisiGoster {
+            for var i = girilenHarf ; i < cevap.characters.count ; ++i {
+                outYanit.text = outYanit.text! + "  -"
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    @IBAction func btnHarfSayisi(sender: AnyObject) {
+        // TODO: yetki odeme puan vs kontrolü
+        
+        harfSayisiGoster = true
+        
+        // yeniden deneme yapılacağı için sıfırlanır---------------------
+        //ilkHarf = true
+        //girilenHarf = 0
+        //kullaniciCevap = ""
+        // --------------------------------------------------------------
+        
+        cevapReset()
+        
+        outYanit.text = "-" // ilk cizgi başında boşluk olamamsı için burada konur
+        
+        for var i = 1 ; i < cevap.characters.count ; ++i {
+            outYanit.text = outYanit.text! + "  -"
+        }
+        
+        
+    }
+    
+    @IBAction func btnCevap(sender: AnyObject) {
+        // TODO: harfgoster true is  cevapdaki harf sayısı kontrol edilerek uyarı çıkartılmalı
+        if outYanit.text == cevap {
+            print("bildiniz")
+            uyariMesaji("Tebrikler :)", str: "Bildiniz ....")
+            puan = puan + 10
+            
+            
+        } else {
+        
+            print("tekrar deneyiniz")
+            uyariMesaji("Malesef :( ", str: "Tekrar Deneyiniz")
+            
+            puan = puan - 1
+            
+            cevapReset()
+        }
+        
+        outPuan.text = String(puan)
+    }
+
+    @IBOutlet weak var outYanit: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -54,7 +209,12 @@ class ViewController: UIViewController {
                                 }
                             
                                 self.img_file = self.degerArray[0][0]
-                                print("diziden alınan1 \(self.img_file)")
+                                self.cevap = self.degerArray[0][1]
+                                self.harfler = self.degerArray[0][2]
+                                self.harfSayisi = self.cevap.characters.count
+                                    
+                                    
+                                print("diziden alınan1 \(self.img_file) \(self.cevap)")
                             
                             } else {
                                 print("web servisi anlamlı data cevirmedi \(webContent)")
@@ -64,31 +224,35 @@ class ViewController: UIViewController {
                             //--------------------image alınacak
                             //--------------------------------------------------------------------
                             
-                        
+                            //TODO: image alınamadıysa uyarı vermeli ve buttonlara basılamamalı.
                             
-                            let img_url = NSURL(string: self.img_path_prefix + self.img_file)
-                            print(" aranan \(self.img_path_prefix) \(self.img_file) ")
-        
-                            let task2 = NSURLSession.sharedSession().dataTaskWithURL(img_url!) { (data, response, error) -> Void in
-                
-                                if error != nil {
-                                    print(error)
-                                } else {
-                                    var documentsDirectory:String?
-                                    var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
-                    
-                                    if paths.count > 0 {
-                                        documentsDirectory = paths[0] as? String
-                                        let savePath = documentsDirectory! + self.img_file
-                                        NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
-                                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                                            self.image.image = UIImage(named: savePath)
-                                        })
-                                    }
-                                }
-                            }
-        
-                            task2.resume()
+                            self.getImage(self.img_path_prefix, fileName: self.img_file)
+                            
+//                            let img_url = NSURL(string: self.img_path_prefix + self.img_file)
+//                            print(" aranan \(self.img_path_prefix) \(self.img_file) ")
+//        
+//                            let task2 = NSURLSession.sharedSession().dataTaskWithURL(img_url!) { (data, response, error) -> Void in
+//                
+//                                if error != nil {
+//                                    print(error)
+//                                } else {
+//                                    var documentsDirectory:String?
+//                                    var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+//                    
+//                                    if paths.count > 0 {
+//                                        documentsDirectory = paths[0] as? String
+//                                        let savePath = documentsDirectory! + self.img_file
+//                                        NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
+//                                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                                            self.image.image = UIImage(named: savePath)
+//                                        })
+//                                    }
+//                                    
+//                                    print("cevap: \(self.cevap) harfler : \(self.harfler)")
+//                                }
+//                            }
+//        
+//                            task2.resume()
                             
                             
                             //--------------------------------------------------------------------
